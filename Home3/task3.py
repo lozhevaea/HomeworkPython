5a8fdb512554d89a2d2286b2f9a20a7d1462920a
Elena Kuznetsova <lozhevaea@gmail.com>
2023-07-25 21:39:19 +0500

    add new task

--- Home3/task3.py
+++ Home3/task3.py
@@ -83,3 +83,19 @@ print(res)
 #         res += int(p)
 # print(res)
     
+
+
+# my_dict={1:'A, E, I, O, U, L, N, S, T, R,А, В, Е, И, Н, О, Р, С, Т',
+#          2:'D, G,Д, К, Л, М, П, У ',
+#          3:'B, C, M, P,Б, Г, Ё, Ь, Я',
+#          4:'F, H, V, W, Y,Й, Ы',
+#          5:'K Ж, З, Х, Ц, Ч',
+#          8:'J, X Ш, Э, Ю',
+#          10:'Q, Z,Ф, Щ, Ъ'}
+# k = 'ноутбук'
+# count=0
+# for elem in k:
+#         for key,value in my_dict.items():
+#             if elem.upper() in value:
+#                 count+=key
+# print(f'World value:{count}')
--- Home4/task1.py
+++ Home4/task1.py
@@ -3,11 +3,9 @@
 # Input: a a a b c a a d c d d
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 
-str = input('Введите буквы через пробел: ').split()
-s = str
+str = tuple(input('Введите буквы через пробел: ').split())
+s = list(str)
 m = set(str)
-str = tuple(str)
-
 for j in m:
     n = 0
     for i in range(len(str)):
@@ -17,3 +15,13 @@ for j in m:
             s[i] = f'{str[i]}_{n}'
             n += 1
 print(*s)
+
+
+# str = input('Введите строку: ').split()
+# result = {}
+# for i in str:
+#     if i in result:
+#         print(f'{i}_{result[i]}', end=' ')
+#     else:
+#         print(i, end=' ')
+#     result[i] = result.get(i, 0) + 1
