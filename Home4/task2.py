2abfb809053d20b394dc6dbafa655400041d193d
Elena Kuznetsova <lozhevaea@gmail.com>
2023-07-30 13:33:00 +0500

    add task24

--- Home4/task2.py
+++ Home4/task2.py
@@ -2,12 +2,10 @@
 # подряд, слова разделены одним или большим числом пробелов. Определите, сколько различных слов 
 # содержится в этом тексте.
 
-# Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
+# Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
 
 # Output: 13
 
 str = set(input('Введите строку: ').split())
 print(str)
 print(len(str))
-
-
